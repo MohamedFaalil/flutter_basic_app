308636270850c4818f3710ffe699f8fd06bf6101
Faalil <mohamedfaalil05@gmail.com>
2022-08-21 18:11:55 +0530

    seperated state changable and non changable widgets for performance optimization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:basic_app/result.dart';
 import 'package:flutter/material.dart';
-import './quiz.dart';
+import './quiz_collection.dart';
 
 // void main(List<String> args) {
 //   runApp(BasicApp());
@@ -8,106 +7,8 @@ import './quiz.dart';
 
 void main() => runApp(const BasicApp());
 
-class BasicApp extends StatefulWidget {
+class BasicApp extends StatelessWidget {
   const BasicApp({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return _BasicAppState();
-  }
-}
-
-class _BasicAppState extends State<BasicApp> {
-  //here _questions variable can be initialized with a value only one time
-  // also its value can not be modified too due to const key
-  final _questions = const [
-    {
-      'questionText': "What's your favorite color?",
-      'answers': [
-        {
-          'text': 'Black',
-          'score': 13,
-        },
-        {
-          'text': 'Red',
-          'score': 27,
-        },
-        {
-          'text': 'Green',
-          'score': 35,
-        },
-        {
-          'text': 'White',
-          'score': 25,
-        },
-      ],
-    },
-    {
-      'questionText': "What's your favorite animal?",
-      'answers': [
-        {
-          'text': 'Cat',
-          'score': 30,
-        },
-        {
-          'text': 'Horse',
-          'score': 20,
-        },
-        {
-          'text': 'Tiger',
-          'score': 10,
-        },
-        {
-          'text': 'Rabbit',
-          'score': 30,
-        },
-        {
-          'text': 'Elephant',
-          'score': 10,
-        },
-      ],
-    },
-    {
-      'questionText': "What's your favorite instructor?",
-      'answers': [
-        {
-          'text': 'Max',
-          'score': 25,
-        },
-        {
-          'text': 'Mosh',
-          'score': 25,
-        },
-        {
-          'text': 'Krish',
-          'score': 10,
-        },
-        {
-          'text': 'Caleb',
-          'score': 25,
-        },
-        {
-          'text': 'Navin',
-          'score': 15,
-        },
-      ],
-    },
-  ];
-  var _questionIndex = 0;
-  double _totalScore = 0.0;
-  void _answerQuestion(double selected_answers_score) {
-    setState(() {
-      _questionIndex += 1;
-      _totalScore += selected_answers_score;
-    });
-  }
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -127,9 +28,7 @@ class _BasicAppState extends State<BasicApp> {
         appBar: AppBar(
           title: const Text('This is my first app'),
         ),
-        body: _questions.length > _questionIndex
-            ? Quiz(_questions, _questionIndex, _answerQuestion)
-            : Result(_totalScore, _resetQuiz),
+        body: const QuizCollection(),
       ),
     );
   }
---
+++ lib/quiz_collection.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+class QuizCollection extends StatefulWidget {
+  const QuizCollection({Key? key}) : super(key: key);
+
+  @override
+  State<QuizCollection> createState() => _QuizCollectionState();
+}
+
+class _QuizCollectionState extends State<QuizCollection> {
+  //here _questions variable can be initialized with a value only one time
+  // also its value can not be modified too due to const key
+  final _questions = const [
+    {
+      'questionText': "What's your favorite color?",
+      'answers': [
+        {
+          'text': 'Black',
+          'score': 13,
+        },
+        {
+          'text': 'Red',
+          'score': 27,
+        },
+        {
+          'text': 'Green',
+          'score': 35,
+        },
+        {
+          'text': 'White',
+          'score': 25,
+        },
+      ],
+    },
+    {
+      'questionText': "What's your favorite animal?",
+      'answers': [
+        {
+          'text': 'Cat',
+          'score': 30,
+        },
+        {
+          'text': 'Horse',
+          'score': 20,
+        },
+        {
+          'text': 'Tiger',
+          'score': 10,
+        },
+        {
+          'text': 'Rabbit',
+          'score': 30,
+        },
+        {
+          'text': 'Elephant',
+          'score': 10,
+        },
+      ],
+    },
+    {
+      'questionText': "What's your favorite instructor?",
+      'answers': [
+        {
+          'text': 'Max',
+          'score': 25,
+        },
+        {
+          'text': 'Mosh',
+          'score': 25,
+        },
+        {
+          'text': 'Krish',
+          'score': 10,
+        },
+        {
+          'text': 'Caleb',
+          'score': 25,
+        },
+        {
+          'text': 'Navin',
+          'score': 15,
+        },
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  double _totalScore = 0.0;
+  void _answerQuestion(double selected_answers_score) {
+    setState(() {
+      _questionIndex += 1;
+      _totalScore += selected_answers_score;
+    });
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _questions.length > _questionIndex
+        ? Quiz(_questions, _questionIndex, _answerQuestion)
+        : Result(_totalScore, _resetQuiz);
+  }
+}
